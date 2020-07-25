a43d620ba3f33fd13a5c07ab704c4cf9a35b8b87
fulinliu <fulinliu@tencent.com>
2020-07-25 16:41:35 +0800

    ‘联调’

--- app.js
+++ app.js
@@ -2,38 +2,80 @@
 App({
   onLaunch: function () {
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // var logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
 
     // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
+    console.log('before login')
+    let wxlogin = new Promise((resolved,reject) =>{
+      wx.login({
+        success: res => {
+          this.globalData.code = res.code
+          resolved()
+        } 
+      })
     })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+    let getSetting = new Promise((resolved,reject) =>{
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              success: res => {
+                // 可以将 res 发送给后台解码出 unionId
+                this.globalData.userInfo = res.userInfo
+                resolved()
+                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                // 所以此处加入 callback 以防止这种情况
+                if (this.userInfoReadyCallback) {
+                  this.userInfoReadyCallback(res)
+                }
               }
-            }
-          })
+            })
+          }
         }
+      })
+    })
+    Promise.all([wxlogin, getSetting]).then(_=> {
+      this.getToken()
+    })
+    this.Request
+  },
+  Request(options){
+    console.log(this.globalData)
+    return wx.request({
+      ...options,
+      url: 'http://47.100.98.138/v1/' + options.url + '?token=' + this.globalData.token + '&uid='+ this.globalData.uid,
+      data: JSON.stringify({
+        token: this.globalData.token,
+        uid: this.globalData.uid
+      })
+    })
+  },
+  getToken(){
+    wx.request({
+      url: this.globalData.publishUrl + '/user/wechat_login',
+      method: 'POST',
+      header: {'content-type': 'application/json'},
+      data: JSON.stringify({
+        "code": this.globalData.code,
+        "nick_name":  this.globalData.userInfo.nick_name 
+      }),
+      success: (res)=> {
+        console.log(res)
+        console.log(11111111111111111111111111111111111)
+        let token = res.data.data.token
+        this.globalData.token = token
+        this.globalData.uid = res.data.data.uid
       }
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    code: null,
+    token: null,
+    publishUrl: 'https://liufulin.xyz/v1/',
+    uid: undefined,
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,12 +6,12 @@ Page({
   data: {
     lifeTime: 0,
     penguinName: '',
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    ani: null,
+    ani: false,
     showAni: false,
+    logs:[{"id":1,"life":10,"nick_name":"轻罗小扇"},{"id":1,"life":10,"nick_name":"轻罗小扇"},{"id":1,"life":10,"nick_name":"轻罗小扇"},{"id":1,"life":20,"nick_name":"你是弟弟"},{"id":1,"life":2,"nick_name":"谁是弟弟"}],
   },
   //事件处理函数
   bindViewTap: function() {
@@ -20,92 +20,137 @@ Page({
     })
   },
   // 喂养函数
-  eat: function() {
-    // this.setData({
-    //   showAni: true,
-    // })
-    let life = Math.min(30000 , this.data.lifeTime + 1000)
+  setFeedAni(){
     this.setData({
-      lifeTime: life,
+      ani: true 
     })
-    var animation = wx.createAnimation({
-      duration: 1000,
-      timingFunction: 'ease',
-      delay: 0
-    });
-    animation.opacity(0.2).bottom('50%').step()
-    this.setData({
-      ani:  animation.export()
-    })
-    var returnAni = wx.createAnimation({
-      duration: 0,
-      timingFunction: 'ease',
-      delay: 0
-    });
-    returnAni.bottom('-50%').step()
     let timer = setTimeout(() => {
-      clearTimeout(timer)
       this.setData({
-        ani:  returnAni.export()
+        ani: false 
       })
+      clearTimeout(timer)
     }, 1000);
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+  eat: function() {
+    new Promise((resolve,reject) => {
+      app.Request({
+        url: `penguin/feed`,
+        method: 'POST',
+        success: res=>{
+          console.log(res.data.data)
+          if(res.data.data.code === 1){
+            wx.showToast({
+              title: '企鹅死了!',
+              icon: 'none',
+              duration: 2000//持续的时间
+            })
+            return
+          }
+          if(res.data.data.code === 2){
+            wx.showToast({
+              title: '点太快了，等一段时间后再喂养吧！',
+              icon: 'none',
+              duration: 2000//持续的时间
+            })
+            return
+          }
+          this.setFeedAni()
+          wx.showToast({
+            title: '成功',
+            icon: 'success',
+            duration: 2000//持续的时间
+          })
+        }
       })
-      this.getPenguinLife()
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+    })
+  },
+  onLoad: function () {
+    let ws = wx.connectSocket({
+      url: 'ws://47.100.98.138/v1/ws',
+    })
+    ws.onOpen(()=>{ console.log('链接成功res！') });
+    ws.onMessage((res) => {
+      let data = JSON.parse(res.data)
+      this.setData({
+        lifeTime: data.penguin_life,
+        penguinName: 'lfl的企鹅',
+      })
+    })
+    console.log(app.globalData)
+    // 获取用户userid
+    new Promise((resolve,reject) => {
+      if (app.globalData.userInfo) {
+        console.log('存在userinfo')
         this.setData({
-          userInfo: res.userInfo,
+          userInfo: app.globalData.userInfo,
           hasUserInfo: true
         })
-        this.getPenguinLife()
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
+        resolve()
+      } else if (this.data.canIUse){
+        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+        // 所以此处加入 callback 以防止这种情况
+        app.userInfoReadyCallback = res => {
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
-          this.getPenguinLife()
+          resolve()
         }
-      })
-    }
+      } else {
+        // 在没有 open-type=getUserInfo 版本的兼容处理
+        wx.getUserInfo({
+          success: res => {
+            app.globalData.userInfo = res.userInfo
+            this.setData({
+              userInfo: res.userInfo,
+              hasUserInfo: true
+            })
+            resolve()
+          }
+        })
+      }
+    }).then(_=> {
+      console.log(22222222222222222222222)
+      console.log(this.data.userInfo.nickName)
+      app.getToken()
+    })
   },
+  // 获取用户信息
   getUserInfo: function(e) {
-    console.log(e)
+    console.log('getUserInfo')
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
   },
-  // 拉去企鹅信息函数
-  getPenguinLife(){
-    this.setData({
-      lifeTime: 20000,
-      penguinName: 'lfl的企鹅',
-    })
-    let timer = setInterval(_=>{
-      let life = this.data.lifeTime - 100
-      if(life < 0){
-        clearInterval(timer)
-      }
-      else{
-        this.setData({
-          lifeTime: this.data.lifeTime - 100,
-          penguinName: 'lfl的企鹅',
-        })
-      }
-    },100)
-  }
+  onShareAppMessage: function( options ){
+    　　var that = this;
+    　　var shareObj = {
+    　　　　title: "一起养企鹅吧！",        // 默认是小程序的名称(可以写slogan等)
+    　　　　path: '/pages/index/index',        // 默认是当前页面，必须是以‘/’开头的完整路径
+    　　　　success: function(res){
+    　　　　　　// 转发成功之后的回调
+    　　　　　　if(res.errMsg == 'shareAppMessage:ok'){
+    　　　　　　}
+    　　　　},
+    　　　　fail: function(){
+    　　　　　　// 转发失败之后的回调
+    　　　　　　if(res.errMsg == 'shareAppMessage:fail cancel'){
+    　　　　　　　　// 用户取消转发
+    　　　　　　}else if(res.errMsg == 'shareAppMessage:fail'){
+    　　　　　　　　// 转发失败，其中 detail message 为详细失败信息
+    　　　　　　}
+    　　　　},
+    　　};
+    　　// 来自页面内的按钮的转发
+    　　if( options.from == 'button' ){
+    　　　　var eData = options.target.dataset;
+    　　　　console.log( eData.name );     // shareBtn
+    　　　　// 此处可以修改 shareObj 中的内容
+    　　　　shareObj.path = '/pages/btnname/btnname?btn_name='+eData.name;
+    　　}
+    　　// 返回shareObj
+    　　return shareObj;
+    }
 })
